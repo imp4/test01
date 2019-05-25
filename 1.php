04d2738da560cbf1ecb11fa71f24fc2b1d11d7c4
imp4 <icecren@gmail.com>
2019-05-25 23:15:14 +0800

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,25 @@
+﻿<?php
+header("Content-Type:text/html; charset=utf-8");
+$con = mysqli_connect("sql12.freemysqlhosting.net","sql12293251","WPzLK2JHir","sql12293251");
+ 
+// Check connection
+if (mysqli_connect_errno())
+ {
+ echo "無法連線到MySQL: " . mysqli_connect_error();
+ }
+ 
+// Perform queries
+mysqli_query($con,"SET CHARACTER SET UTF8");
+$sql="SELECT * FROM mouse ORDER BY phone DESC, address DESC,product DESC,total DESC";
+$result = mysqli_query($con,$sql);
+ 
+$json = array();
+if(mysqli_num_rows($result)){
+ while($row=mysqli_fetch_assoc($result)){
+   $json[]=$row;
+ }
+}
+echo urldecode(json_encode($json));
+ 
+mysqli_close($con);
+?>
---
+++ update.php
@@ -0,0 +1,27 @@
+﻿<?php
+header("Content-Type:text/html; charset=utf-8");
+$con = mysqli_connect("sql12.freemysqlhosting.net","sql12293251","WPzLK2JHir","sql12293251");
+// Check connection
+if (mysqli_connect_errno())
+ {
+ echo "無法連線到MySQL: " . mysqli_connect_error();
+ }
+ 
+$user = $_POST['user'];
+$phone = $_POST['phone'];
+$address = $_POST['address'];
+$product = $_POST['product'];
+$total = $_POST['total']; 
+ 
+// Perform queries
+$sql="INSERT INTO mouse (user, phone, address,product,total) VALUES (CONVERT(_utf8 '$user' USING utf8), '$phone', '$address','$product','$total')";
+ 
+if (mysqli_query($con,$sql)){
+  echo "您的資料上傳成功";
+}
+else {
+  echo "您的資料上傳失敗";
+}
+ 
+mysqli_close($con);
+?>
